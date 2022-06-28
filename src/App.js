4c8308e220f19fd52766c3c3ce0629afb0c22ed1
Jubayer Juhan <33898452+jubayerjuhan@users.noreply.github.com>
2022-06-28 06:29:34 +0000

    Maps added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useRef, useEffect, useState } from "react";
+import mapboxgl from "mapbox-gl";
+import "./app.css";
 const App = () => {
-  const dispatch = useDispatch()
-  const username = useSelector(state => state.username)
-  // 
-  const dispatcher = () => {
-    dispatch({ type: 'CHANGE_USERNAME', payload: 'Juhanners' })
-  }
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoianViYXllcmp1aGFuIiwiYSI6ImNrdXI0cHFseTE4bWIyb3FybDdreWNpNDQifQ.ZlyPUhzJalEYFiinkw1ydg";
+
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+  });
+
   return (
     <div>
-      <h1>{username}</h1>
-      <button onClick={dispatcher}>Click Me To Lunch The Payload</button>
-      {/* <input type="text" value={username} onChange={e => dispatch({type: 'CHANGE_USERNAME', })} /> */}
+      <div ref={mapContainer} className="map-container" />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
