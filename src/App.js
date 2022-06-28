5e79b68ac1494028bbbc8a12ec8aeca828b6e5d7
Jubayer Juhan <33898452+jubayerjuhan@users.noreply.github.com>
2022-06-28 06:34:59 +0000

    Package edited

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v11",
+      style: "mapbox://styles/jubayerjuhan/cl4xsjx3w000h14uis0dyzjou",
       center: [lng, lat],
       zoom: zoom,
     });
