14e2fb6317fc89e6d30f3f3e3f889c6ad5b48009
Jubayer Juhan <33898452+jubayerjuhan@users.noreply.github.com>
2022-06-28 06:46:51 +0000

    PaNew map

--- src/App.js
+++ src/App.js
@@ -1,40 +1,49 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import "./app.css";
 
-const App = () => {
-  // eslint-disable-next-line import/no-webpack-loader-syntax
-  mapboxgl.workerClass =
-    // eslint-disable-next-line import/no-webpack-loader-syntax
-    require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA";
 
-  //
-  mapboxgl.accessToken =
-    "pk.eyJ1IjoianViYXllcmp1aGFuIiwiYSI6ImNrdXI0cHFseTE4bWIyb3FybDdreWNpNDQifQ.ZlyPUhzJalEYFiinkw1ydg";
+const Map = () => {
+  const mapContainerRef = useRef(null);
 
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
+  const [lng, setLng] = useState(5);
+  const [lat, setLat] = useState(34);
+  const [zoom, setZoom] = useState(1.5);
 
+  // Initialize map when component mounts
   useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/jubayerjuhan/cl4xsjx3w000h14uis0dyzjou",
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: "mapbox://styles/mapbox/streets-v11",
       center: [lng, lat],
       zoom: zoom,
     });
-  });
+
+    // Add navigation control (the +/- zoom buttons)
+    map.addControl(new mapboxgl.NavigationControl(), "top-right");
+
+    map.on("move", () => {
+      setLng(map.getCenter().lng.toFixed(4));
+      setLat(map.getCenter().lat.toFixed(4));
+      setZoom(map.getZoom().toFixed(2));
+    });
+
+    // Clean up on unmount
+    return () => map.remove();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <div>
-      <div ref={mapContainer} className="map-container" />
+      <div className="sidebarStyle">
+        <div>
+          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+        </div>
+      </div>
+      <div className="map-container" ref={mapContainerRef} />
     </div>
   );
 };
 
-export default App;
+export default Map;
